953b11cc1e95535b4ae3f5352794599698d8666c
Ayush Tayal <148463849+AyushTayal777@users.noreply.github.com>
2025-07-24 14:41:43 +0000

    code

--- vitereact/src/main.jsx
+++ vitereact/src/main.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { StrictMode } from 'react'
-
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App!</h1>
+    </div>
+  )
+}
+const anotherElement=(
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <MyApp/>
+    {anotherElement}
   </StrictMode>,
 )
